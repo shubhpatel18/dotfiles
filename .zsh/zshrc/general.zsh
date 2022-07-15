60c99b30403ff465fa68b82c240f1748468f450c
Shubh Patel <shubhp18@gmail.com>
2022-07-15 19:22:41 -0400

    Adjusted completion matching

--- .zsh/zshrc/general.zsh
+++ .zsh/zshrc/general.zsh
@@ -1,5 +1,5 @@
 zstyle ':completion:*' menu select
-zstyle ':completion:*' matcher-list '' 'm:{a-zA-Z}={A-Za-z}' 'r:|=*' 'l:|=* r:|=*'
+zstyle ':completion:*' matcher-list '' 'm:{a-zA-Z}={A-Za-z}' 'r:|[._-]=* r:|=*' 'l:|=* r:|=*'
 autoload -Uz compinit && compinit
 
 HISTFILE=~/.histfile
