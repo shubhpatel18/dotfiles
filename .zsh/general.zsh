1b15aeae750a70a4e3d1b1ee6d66b916d2bdfed4
Shubh Patel <Shubh.Patel@cognosante.com>
2022-07-19 16:19:31 -0400

    Ignore duplicates in history

--- .zsh/general.zsh
+++ .zsh/general.zsh
@@ -2,6 +2,7 @@ zstyle ':completion:*' menu select
 zstyle ':completion:*' matcher-list '' 'm:{a-zA-Z}={A-Za-z}' 'r:|[._-]=* r:|=*' 'l:|=* r:|=*'
 autoload -Uz compinit && compinit
 
+setopt hist_ignore_all_dups
 HISTFILE=~/.histfile
 HISTSIZE=1000
 SAVEHIST=1000
