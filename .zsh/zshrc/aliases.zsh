ed482d3d57b53837749a1d19362c149b8a38a8bb
Shubh Patel <Shubh.Patel@cognosante.com>
2022-08-08 12:42:08 -0400

    Added alias for tree command

--- .zsh/zshrc/aliases.zsh
+++ .zsh/zshrc/aliases.zsh
@@ -1,5 +1,9 @@
+### HISTORY ##################################################################
+
 alias h='history'
 alias hg='history 0 | grep'
+
+#### CHANGE DIRECTORY ########################################################
 
 alias ..='cd ..'
 alias ...='cd ../..'
@@ -8,11 +12,34 @@ alias .....='cd ../../../..'
 alias ......='cd ../../../../..'
 alias cd..='cd ..'
 
+cdl() { cd "$@"; ls; }
+cdla() { cd "$@"; la; }
+cdll() { cd "$@"; ll; }
+cdlh() { cd "$@"; lh; }
+cdllh() { cd "$@"; llh; }
+
+### SHOW FILES ###############################################################
+
+alias tree='tree -F'
+
+t() {
+	if [ $# = 0 ] ; then
+		tree -aL 1
+	elif [ $# = 1 ] ; then
+		tree -aL $1
+	else
+		echo "USAGE: t <depth, default=1>"
+		return 1
+	fi
+}
+
 alias ls='ls -F'
 alias la='ls -A'
 alias ll='ls -Al'
 alias lh='ls -d .*'
 alias llh='ls -dl .*'
+
+### AUTOCOLOR ################################################################
 
 if [ -x /usr/bin/dircolors ] ; then
 	test -r ~/.dircolors && eval "$(dircolors -b ~/.dircolors)" || eval "$(dircolors -b)"
@@ -24,13 +51,8 @@ if [ -x /usr/bin/dircolors ] ; then
 	alias egrep='egrep --color=auto'
 fi
 
-cdl() { cd "$@"; ls; }
-cdla() { cd "$@"; la; }
-cdll() { cd "$@"; ll; }
-cdlh() { cd "$@"; lh; }
-cdllh() { cd "$@"; llh; }
+### TMUX SHORTCUTS ###########################################################
 
-# tmux shortcuts
 alias ttc='tmux choose-window'
 
 ttw() {
@@ -52,3 +74,5 @@ ttp() {
 }
 alias tth='tmux splitw -h'
 alias ttv='tmux splitw -v'
+
+##############################################################################
