2235f049a4b56c20c9ad753f943a99141b77ec86
Shubh Patel <Shubh Patel>
2022-08-13 21:42:18 -0400

    Touch up ssh-agent logic (keychain)

--- .zsh/zprofile/keychain.zsh
+++ .zsh/zprofile/keychain.zsh
@@ -3,17 +3,17 @@ keychain() {
 		if [ -n "$(pgrep -u $USER ssh-agent)" ] ; then
 			export SSH_AGENT_PID="$(pgrep -u $USER ssh-agent)"
 			export SSH_AUTH_SOCK="$(find /tmp/ssh-* -name \
-			"agent.$(expr $SSH_AGENT_PID - 1)")"
+			"agent.$(( $SSH_AGENT_PID - 1 ))")"
 		else
 			# start ssh-agent + keep keys in memory for 12 hours
 			eval $(ssh-agent -t 43200) > /dev/null 2>&1
 		fi
 	fi
 
-	# keep tmux and screens happy by mainting SSH_AUTH_SOCK at
+	# keep tmux and screens happy by maintaining $SSH_AUTH_SOCK at
 	# .ssh/ssh_auth_sock
 	home_sock="$HOME/.ssh/ssh_auth_sock"
-	if [ $SSH_AUTH_SOCK != "$home_sock" ] && [ -S "$SSH_AUTH_SOCK" ] ; then
+	if [ "$SSH_AUTH_SOCK" != "$home_sock" ] && [ -S "$SSH_AUTH_SOCK" ] ; then
 		ln -sf "$SSH_AUTH_SOCK" "$home_sock"
 	fi
 	export SSH_AUTH_SOCK="$home_sock"
