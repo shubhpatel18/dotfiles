dedc7a9d5f9236ebc217e9575e6de5b9f5c26dab
Shubh Patel <shpatel@epic.com>
2026-03-30 12:25:49 -0500

    Move ws to wsl.zsh, and remove obsidian

--- .zsh/zshrc/wsl.zsh
+++ .zsh/zshrc/wsl.zsh
@@ -1,3 +1,10 @@
 [[ -z "$WSL_DISTRO_NAME" ]] && return
 
 alias explorer='explorer.exe'
+
+# Open a folder in Windsurf from WSL
+ws() {
+  local folder
+  folder=$(readlink -f "${1:-.}")
+  windsurf --folder-uri "vscode-remote://wsl+${WSL_DISTRO_NAME}${folder}"
+}
