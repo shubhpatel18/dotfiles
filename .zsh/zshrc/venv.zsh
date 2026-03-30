9886c8028b410a0f304dfd5393b8a4d84cbdb420
Shubh Patel <shpatel@epic.com>
2026-03-30 10:03:03 -0500

    Add new venv aliases

--- .zsh/zshrc/venv.zsh
+++ .zsh/zshrc/venv.zsh
@@ -2,5 +2,7 @@ alias venv='echo "venv: $VIRTUAL_ENV"'
 alias venv3.8="python3.8 -m venv ./venv"
 alias venv3.9="python3.9 -m venv ./venv"
 alias venv3.10="python3.10 -m venv ./venv"
+alias venv3.11="python3.11 -m venv ./venv"
+alias venv3.12="python3.12 -m venv ./venv"
 
 alias activate="source venv/bin/activate"
